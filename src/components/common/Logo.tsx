86eaf0f9844e50b7034b2e0eb2f5225202d48aee
Soumya Parida <soumyaparida00@gmail.com>
2025-07-22 12:18:51 +0530

    some fixes

--- src/components/common/Logo.tsx
+++ src/components/common/Logo.tsx
@@ -1,13 +1,14 @@
-// components/common/Logo.tsx
 import React from 'react';
+import logoImage from '../../assets/images/Virtual Tech Box (1) (1).png';
 
 const Logo: React.FC = () => {
   return (
     <div className="flex items-center space-x-2">
-      <div className="w-10 h-10 bg-white bg-opacity-20 rounded-lg flex items-center justify-center">
-        {/* Virtual Tech Box */}
-        <span className="text-xl font-bold">VTB</span>
-      </div>
+      <img 
+        src={logoImage} 
+        alt="Virtual Tech Box Logo" 
+        className="w-10 h-10 rounded-lg"
+      />
       <span className="text-xl font-semibold hidden sm:block">Virtual Tech Box</span>
     </div>
   );
